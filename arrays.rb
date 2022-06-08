a2c5912d1babd4b782afddf7a2e7c8cf2966e8a6
Jake Kim <imjakekim@gmail.com>
2022-06-08 08:18:25 -0700

    Add 4 array methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ fish = ["one fish", "two fish", "red fish", "blue fish"]
 primes = [2, 3, 5, 7, 11]
 dollar_bills = [1.00, 2.00, 5.00, 10.00]
 judge_rulings = [true, true, true, false, false]
+
+#Removes and returns the last 2 elements of fish
+fish.pop(2)
+
+#Adds 13 & 17 to the end of primes
+primes.push(13, 17)
+
+#Removes and returns the first 2 elements of dollar_bills
+dollar_bills.shift(2)
+
+#Adds 2 elemnts to the beginning of judge_rulings
+judge_rulings.unshift(false, false)
